e0a98a99a58658f6740ae8dfa077b51e58ac7a78
Shiv Baran Singh <spyshiv@gmail.com>
2025-04-20 19:27:48 +0530

    Code refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import Hero from "@/components/home/hero-section";
 import About from "@/components/home/about-section";
 import Projects from "@/components/home/project-section";
-import { NavbarMenu } from "@/components/shared/navbar";
-import { Dock } from "@/components/shared/dock";
 
 export default function Home() {
   return (
